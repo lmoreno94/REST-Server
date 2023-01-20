af0c7ba7c5c10f7bfcefdf9d58c17b577aa57168
IsMor3Var <lmoreno9943@gmail.com>
2023-01-19 21:04:36 -0500

    Databse Setup

--- Server_GraphQL/src/db/connect.ts
+++ Server_GraphQL/src/db/connect.ts
@@ -2,7 +2,7 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: "mysql",
     port: 3306,
     username: "root",
     password: "root123456",
--- Server_GraphQL/src/index.ts
+++ Server_GraphQL/src/index.ts
@@ -1,9 +1,16 @@
 import "reflect-metadata";
 import app from "./app";
+import { AppDataSource } from "./db/connect";
 
-const PORT = 3000;
+const PORT = 3001;
 
 (async() => {
+    await AppDataSource.initialize().then(() => {
+        console.log('Connect DB');
+    }).catch((err) => {
+        console.log('Error Connect DB', err);
+    });
+    
     app.listen(PORT, () => {
         console.log(`Server on port ${PORT}`);
     })
