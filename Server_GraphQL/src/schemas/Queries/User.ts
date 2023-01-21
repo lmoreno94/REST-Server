4e1e748494590610a88ebd5573421fa18cd06c6c
IsMor3Var <lmoreno9943@gmail.com>
2023-01-21 18:24:42 -0500

    Query get One User by id

--- Server_GraphQL/src/schemas/Queries/User.ts
+++ Server_GraphQL/src/schemas/Queries/User.ts
@@ -1,11 +1,28 @@
 import { TUser } from '../TypeDefs/User';
 import { Users } from '../../entities/User';
-import { GraphQLList } from 'graphql';
+import { GraphQLList, GraphQLID } from 'graphql';
 
 export const GET_ALL_USERS = {
     type: new GraphQLList(TUser),
     async resolve(){
         const result = await Users.find();
+        return result;
+    }
+}
+
+export const GET_ONE_USER = {
+    type: TUser,
+    args: {
+        id: {
+            type: GraphQLID
+        }
+    },
+    async resolve(_: any, args: any){
+        console.log(args);
+        
+        const result = await Users.findOneBy({ id: args.id });
+        console.log(result);
+        
         return result;
     }
 }
